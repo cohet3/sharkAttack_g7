97c88d3374914c8e45634714005a84d6d3efa412
Luis H Rodríguez <160022546+LuisHRF@users.noreply.github.com>
2024-09-05 15:55:23 +0200

    Subida de los comentarios

--- functions.py
+++ functions.py
@@ -6,21 +6,29 @@ import warnings as wrn
 import pandas as pd
 
 
-def clean_columns_names(data_frame, columns_to_drop=None): #
+def clean_columns_names(data_frame): 
+    """
+    Matches the formats of all column names: replaces blanks with '_' and converts them to lowercase
+    """
     
     data_frame.columns = data_frame.columns.str.replace(' ', '_').str.lower()
 
     return data_frame
 
-def columns_drops(data_frame, columns_to_drop=None): #
+def columns_drops(data_frame, columns_to_drop=None): 
+    """
+    Allows you to delete columns
+    """
     
     if columns_to_drop is not None:
         data_frame = data_frame.drop(columns=columns_to_drop, errors='ignore')
 
     return data_frame
 
-def drop_rows_nulls(data_frame, thresh=2): #
-
+def drop_rows_nulls(data_frame, thresh=2): 
+    """
+    Allows you to delete rows that contain less than a minimum value of valid values
+    """
     if thresh < 1:
         raise ValueError('Threshold must be 1 or less.') 
         
@@ -30,6 +38,9 @@ def drop_rows_nulls(data_frame, thresh=2): #
     return data_frame_clean
 
 def clean_and_filter_dates(dataframe, column_name, year): # 
+    """
+    This function focuses on cleaning dates: it converts these values ​​into a Year-Month-Day format and transforms it into datetime
+    """
 
     def clean_datetimes(df_general):
 
@@ -44,7 +55,8 @@ def clean_and_filter_dates(dataframe, column_name, year): #
     with wrn.catch_warnings():
         wrn.simplefilter(action='ignore', category=pd.errors.SettingWithCopyWarning)
     
-    dataframe_copy = dataframe.copy()
+    
+    dataframe_copy = dataframe.copy() # .copy() is used to avoid the error of working on the original dataframe
     
     dataframe_copy[column_name] = dataframe_copy[column_name].apply(clean_datetimes)
 
@@ -56,6 +68,9 @@ def clean_and_filter_dates(dataframe, column_name, year): #
     return dataframe_filtered
 
 def categorize_injury(injury_text):
+    """
+    Create a new column in the dataframe that allows splitting the 'Injury' values ​​between fatal and non-fatal
+    """
     injury_text = str(injury_text).lower()
 
     fatal_keywords = ['fatal', 'death', 'died', 'deceased', 'dead']
@@ -66,7 +81,9 @@ def categorize_injury(injury_text):
         return 'No Fatal'
     
 def filter_activity(activity_text):
-
+    """
+    Filters the activities that victims were doing by trying to clean common words
+    """
     if pd.isna(activity_text):
         return "Unknown"
     
@@ -81,7 +98,9 @@ def filter_activity(activity_text):
             return word.capitalize()
 
 def temp_date_df(data_frame, column_name):
-
+    """
+    Create a dataframe with the dates of the attacks divided into year, month and day to facilitate their management
+    """
     data_frame[column_name] = pd.to_datetime(data_frame[column_name])
 
     data_frame_tmp = pd.DataFrame({
@@ -92,8 +111,10 @@ def temp_date_df(data_frame, column_name):
 
     return data_frame_tmp
 
-def generate_case_numbers(data_frame, prefix = 'ND.', start_index = 1): # 
-
+def generate_case_numbers(data_frame, prefix = 'ND.', start_index = 1): 
+    """
+    Creates a column which generates a case number for each attack
+    """
     data_frame_reversed = data_frame.iloc[::-1].reset_index(drop=True)
     
     data_frame_reversed['case_number'] = data_frame_reversed.index + start_index
@@ -101,4 +122,25 @@ def generate_case_numbers(data_frame, prefix = 'ND.', start_index = 1): #
 
     data_frame_final = data_frame_reversed.iloc[::-1].reset_index(drop=True)
 
-    return data_frame_final
+    return data_frame_finalç
+
+def clean_strings(string): 
+    """
+    Function to clean strings by:
+    - taking the first value if it contains a slash
+    - replacing ' and ' with ' & '
+    - removing all non-alphanumeric characters
+    - stripping leading and trailing whitespace
+    Args:
+        string (str): string to clean
+    Returns:
+        str: cleaned string
+    """
+    new_str = string
+    if isinstance(string, str):
+        new_str = new_str.split("/")[0].strip() #when there are two possible values we'll pick the first one
+        new_str = new_str.replace(" and ", " & ").replace(" AND ", " & ") #formatting
+        new_str = re.sub(r"[@!#$%^*?()]", "", new_str)
+        new_str = re.sub(r"\s+", " ", new_str)
+        new_str = new_str.strip()
+    return new_str
